ab85d8ed58bb5c0b103b5014109527fb1cbd97f3
Rafael Batista <rafaelb24@yahoo.com>
2018-10-08 15:17:58 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,13 +77,9 @@ end
 def won?(board)
   WIN_COMBINATIONS.each do |win|
     if board[win[0]] == "X" && board[win[1]] == "X" && board[win[2]] == "X"
-
       return win
     elsif board[win[0]] == "O" && board[win[1]] == "O" && board[win[2]] == "O"
-
       return win
-    else
-      won = false
     end
   end
 
