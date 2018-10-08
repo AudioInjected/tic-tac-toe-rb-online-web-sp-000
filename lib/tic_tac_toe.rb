d2ba8facde327063ac4e0a207d4ed9c0547e758b
Rafael Batista <rafaelb24@yahoo.com>
2018-10-08 14:42:39 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,67 @@
+#rspec spec/01_tic_tac_toe_spec.rb
+WIN_COMBINATIONS = [
+ [0, 1, 2],
+ [3, 4, 5],
+ [6, 7, 8],
+ [0, 3, 6],
+ [1, 4, 7],
+ [2, 5, 8],
+ [0, 4, 8],
+ [2, 4, 6]
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, value)
+  board[index] = value
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board,index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+  end
+end
+
+def turn_count(board)
+counter = 0
+  board.each do |count|
+    if count == "X" || count == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+ turn = turn_count(board)
+  if turn.even?
+    return "X"
+  else
+    return "O"
+  end
+end
